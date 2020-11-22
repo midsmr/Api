5dcad1a7749d9ca7de006b01a0afa231628020f4
Somebody <midsmr@qq.com>
2020-11-22 21:46:35 +0800

    修改首页

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,14 @@
 <html lang="zh">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>教书先生API - 提供免费接口调用平台</title>
-    <meta name="description" content="教书先生API是教书先生免费提供API数据接口调用服务平台 - 我们致力于为用户提供稳定、快速的免费API数据接口服务。">
-    <meta name="keywords" content="API,聚合数据,API数据接口,API,免费接口,免费api接口调用,免费API数据调用,教书先生API">
-    <meta name="author" content="教书先生">
-    <meta name="founder" content="教书先生API">
-    <link href="//ku.oioweb.cn/Bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/oneui.css" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="img/api.png">
+    <title>{{config('site.name')}}: {{config('site.description')}}</title>
+    <meta name="description" content="{{config('site.description')}}">
+    <meta name="keywords" content="{{config('site.keywords')}}">
+    <meta name="homepage" content="https://github.com/midsmr/Api">
+    <link href="{{asset('static/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('static/css/style.css')}}" rel="stylesheet">
 </head>
 <body>
 <header class="site-header">
@@ -33,24 +30,23 @@
 <section class="content content-boxed">
     <p class="p-jsxs">拒绝流量劫持，全面使用 HTTPS！</p>
     <div class="row row_jsxs">
-        <div class="col-md-4"><a target="_blank" class="block block-link-hover2 ribbon ribbon-modern ribbon-success"
-                                 href="api/doc/qq.php">
+        <div class="col-md-4">
+            <a target="_blank"
+               class="block block-link-hover2 ribbon ribbon-modern ribbon-success"
+               href="api/doc/qq.php">
                 <div class="ribbon-box font-w600">调用：65800956</div>
                 <div class="block-content">
                     <div class="h4 push-5">QQ信息获取</div>
-                    <p class="text-muted">解析QQ昵称,邮箱,头像</p></div>
+                    <p class="text-muted">解析QQ昵称,邮箱,头像</p>
+                </div>
             </a>
         </div>
         <div class="col-sm-12">
             <div class="block block-link-hover2 ribbon ribbon-modern ribbon-success">
                 <div class="block-content">
                     <a class="block" href="https://api.oioweb.cn">
-                        <p class="text-center" style="margin-bottom: 10px"><a href="http://beian.miit.gov.cn"
-                                                                              target="_blank">
-                                <script type="text/javascript" src="https://api.oioweb.cn/web/icp.php"
-                                        charset="utf-8"></script>
-                            </a> • 教书先生提供免费API服务
-                        </p>
+                        <p class="text-center" style="margin-bottom: 10px">
+                            <a href="http://beian.miit.gov.cn" target="_blank">{{config('site.icp')}}</a> • {{config('site.name')}}</p>
                         <p class="text-center" style="margin-bottom: 10px">实时调用：174726004 次 · 稳定运行：1115 天 </p>
                         <p class="text-center">反馈或建议请发送邮件至：599928887@qq.com</p>
                     </a>
