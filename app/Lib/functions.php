43a24066b7f2d200efb70828f0bfd66f737fe5c3
Somebody <midsmr@qq.com>
2020-11-22 22:03:10 +0800

    新增加一个助手函数并且增加请求总数统计

--- app/Lib/functions.php
+++ app/Lib/functions.php
@@ -12,13 +12,24 @@ use App\Lib\Helper;
 
 /**
  * Json响应助手函数
- * @author midsmr <midsmr@qq.com>
  * @param int|array $status 状态码
  * @param null|string $message 信息
  * @param null|string|array $result 其他参数
  * @return string
+ * @author midsmr <midsmr@qq.com>
  */
 function json($status, $message = null, $result = null)
 {
     return Helper::json($status, $message, $result);
+}
+
+/**
+ * 获取统计数据助手函数
+ * @param $name
+ * @return int
+ * @author midsmr <midsmr@qq.com>
+ */
+function get_count($name)
+{
+    return \App\Models\Count::get($name);
 }
--- app/Models/Count.php
+++ app/Models/Count.php
@@ -16,27 +16,32 @@ class Count
 {
     /**
      * 获取统计的请求数量
-     * @author midsmr <midsmr@qq.com>
      * @param string $name 唯一名称
      * @return int 请求数量
+     * @author midsmr <midsmr@qq.com>
      */
     public static function get(string $name)
     {
         if (Cache::has($name)) {
             return Cache::get($name);
-        } else {
-            Cache::put($name, 0);
-            return 0;
         }
+        Cache::put($name, 0);
+        return 0;
     }
 
     /**
      * 统计请求数量
-     * @author midsmr <midsmr@qq.com>
      * @param string $name 唯一名称
+     * @author midsmr <midsmr@qq.com>
      */
     public static function increment(string $name)
     {
+        if (Cache::has('all')) {
+            Cache::increment('all');
+        } else {
+            Cache::put('all', 1);
+        }
+
         if (Cache::has($name)) {
             Cache::increment($name);
         } else {
