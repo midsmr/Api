ec43159af07108e3662e12768fce5cbb3d39377f
Somebody <midsmr@qq.com>
2020-11-22 22:05:06 +0800

    网站首页来自:https://api.oioweb.cn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,43 +12,41 @@
     <link href="{{asset('static/css/style.css')}}" rel="stylesheet">
 </head>
 <body>
+<!--本页面来源于:https://api.oioweb.cn-->
 <header class="site-header">
     <nav class="nav_jsxs">
-        <span style="float: left;"><a class="logo_jsxs" href=""></a></span>
-        <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=599928887&amp;site=qq&amp;menu=yes">反馈</a>
-        <a target="_blank" href="https://blog.oioweb.cn">博客</a>
-        <a target="_blank" href="https://gitee.com/yurunsoft/IMI">底层框架</a>
-        <a target="_blank" href="https://www.toolnb.com">爱资料在线工具</a>
+        <a target="_blank" href="https://github.com/midsmr/Api/issues">反馈</a>
     </nav>
     <div class="box-text">
-        <h1>教书先生API</h1>
-        <p>稳定、快速、免费的 API 接口服务<br><span class="package-amount">共收录了 <strong>43</strong> 个接口</span> · <span
-                class="package-amount">今日调用 <strong>324073</strong> 次</span>
+        <h1>{{config('site.name')}}</h1>
+        <p>稳定、快速、免费的 API 接口服务<br>
+            <span class="package-amount">共收录了&nbsp;<strong>{{count(config('list'))}}</strong>&nbsp;个接口</span>
         </p>
     </div>
 </header>
 <section class="content content-boxed">
-    <p class="p-jsxs">拒绝流量劫持，全面使用 HTTPS！</p>
     <div class="row row_jsxs">
-        <div class="col-md-4">
-            <a target="_blank"
-               class="block block-link-hover2 ribbon ribbon-modern ribbon-success"
-               href="api/doc/qq.php">
-                <div class="ribbon-box font-w600">调用：65800956</div>
-                <div class="block-content">
-                    <div class="h4 push-5">QQ信息获取</div>
-                    <p class="text-muted">解析QQ昵称,邮箱,头像</p>
-                </div>
-            </a>
-        </div>
+        @foreach(config('list') as $key => $value)
+            <div class="col-md-4">
+                <a target="_blank"
+                   class="block block-link-hover2 ribbon ribbon-modern ribbon-success"
+                   href="{{route($key)}}">
+                    <div class="ribbon-box font-w600">调用：{{get_count($key)}}</div>
+                    <div class="block-content">
+                        <div class="h4 push-5">{{$value['title']}}</div>
+                        <p class="text-muted">{{$value['description']}}</p>
+                    </div>
+                </a>
+            </div>
+        @endforeach
         <div class="col-sm-12">
             <div class="block block-link-hover2 ribbon ribbon-modern ribbon-success">
                 <div class="block-content">
-                    <a class="block" href="https://api.oioweb.cn">
+                    <a class="block" href="https://github.com/midsmr/Api">
                         <p class="text-center" style="margin-bottom: 10px">
                             <a href="http://beian.miit.gov.cn" target="_blank">{{config('site.icp')}}</a> • {{config('site.name')}}</p>
-                        <p class="text-center" style="margin-bottom: 10px">实时调用：174726004 次 · 稳定运行：1115 天 </p>
-                        <p class="text-center">反馈或建议请发送邮件至：599928887@qq.com</p>
+                        <p class="text-center" style="margin-bottom: 10px">实时调用:&nbsp;{{get_count('all')}}&nbsp;次</p>
+                        <p class="text-center"></p>
                     </a>
                 </div>
             </div>
