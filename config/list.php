14877c39ecb470ef60f4c2a8576093e7771e5f34
midsmr <midsmr@qq.com>
2021-01-15 00:14:18 +0800

    自定义icon

--- config/list.php
+++ config/list.php
@@ -10,16 +10,28 @@
 // +---------------------------------------------------------
 
 return [
+    // title 为页面标题
+    // description 为页面介绍词
+    // icon 为侧边栏按钮的mdi图标 具体查看 http://lyear.itshubao.com/v4/lyear_ui_icon.html
+
     'index' => [
         'title' => '概述',
         'description' => '关于本站',
+        'icon' => 'mdi-home',
+    ],
+    'json' => [
+        'title' => 'JSON响应',
+        'description' => '所有接口JSON响应的公共参数说明',
+        'icon' => 'mdi-json',
     ],
     'icp' => [
         'title' => '备案查询',
         'description' => '查询域名的ICP备案信息',
+        'icon' => 'mdi-clipboard-text',
     ],
     'qrcode' => [
         'title' => '二维码生成',
-        'description' => '生成一个Qr二维码'
+        'description' => '生成一个Qr二维码',
+        'icon' => 'mdi-qrcode',
     ]
 ];
