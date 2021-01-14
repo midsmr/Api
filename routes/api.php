1408868f036c17dcab79eaa10755c2bbea2e6291
midsmr <midsmr@qq.com>
2021-01-14 22:25:33 +0800

    路由修改

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,15 @@
 <?php
+// +---------------------------------------------------------
+// | PHP is the best language in the world
+// +---------------------------------------------------------
+// | Copyright (c) 2020 Somebody All rights reserved.
+// +---------------------------------------------------------
+// | License: http://license.coscl.org.cn/MulanPSL2
+// +---------------------------------------------------------
+// | Author: Somebody <midsmr@qq.com>
+// +---------------------------------------------------------
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('icp', 'IcpController@api')->name('api.icp');
+Route::get('qrcode', 'QrCodeController@api')->name('api.qrcode');
