ea668a44d43c8d0a2130e33756a0a09db2ce3624
midsmr <midsmr@qq.com>
2021-01-15 18:25:07 +0800

    新增加全局请求文档

--- config/list.php
+++ config/list.php
@@ -22,6 +22,11 @@ return [
     'response' => [
         'title' => '全局响应',
         'description' => '所有接口响应的公共参数说明',
+        'icon' => 'mdi-router-wireless',
+    ],
+    'request' => [
+        'title' => '全局请求',
+        'description' => '所有接口请求的公共参数说明',
         'icon' => 'mdi-json',
     ],
     'icp' => [
